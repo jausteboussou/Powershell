189b511b0a9e4dd95d350aedb323d5825611ade9
Jauste Boussou <134558480+jausteboussou@users.noreply.github.com>
2025-03-19 17:04:15 +0100

    dechiffrementsecretSHA256.ps1

--- dechiffrementsecret.ps1
+++ dechiffrementsecret.ps1
@@ -2,21 +2,11 @@ $certThumbprint = "***"
 
 # Récupérer le certificat à partir du magasin de certificats de la machine
 $cert = Get-Item "Cert:\LocalMachine\My\$certThumbprint"
+$encryptedBytes = get-content -path "***"
+# Déchiffrer le secret avec le bon type de padding
+$rsa = $cert.PrivateKey
+$decryptedBytes = $rsa.Decrypt($encryptedBytes, [System.Security.Cryptography.RSAEncryptionPadding]::OaepSHA256)
 
-# Secret chiffré (en Base64)
-$encryptedSecretBase64 = get-content -Path "***"
-
-# Convertir le secret chiffré de Base64 en bytes
-$encryptedBytes = [Convert]::FromBase64String($encryptedSecretBase64)
-
-# Accéder à la clé privée RSA directement
-$rsaProvider = $cert.PrivateKey
-
-# Déchiffrer le secret
-$decryptedBytes = $rsaProvider.Decrypt($encryptedBytes, [System.Security.Cryptography.RSAEncryptionPadding]::OaepSHA1)
-
-# Convertir les bytes déchiffrés en chaîne
+# Convertir les bytes déchiffrés en une chaîne
 $decryptedSecret = [System.Text.Encoding]::UTF8.GetString($decryptedBytes)
-
-# Afficher le secret déchiffré
 Write-Output "Secret déchiffré: $decryptedSecret"
